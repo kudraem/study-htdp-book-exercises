3f762eca33cfa949295c77f80cf008cfbbdb4e9d
Ivan Khimich <himivan@gmail.com>
2021-08-02 05:27:45 +0300

    Added exercise-4 solution

---
+++ i-fixed-size-data/exercise-4/solution.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname solution) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(define str "helloworld")
+(define i 10)
+
+(cond
+  [(< i (string-length str)) (string-append (substring str 0 i)
+                                            (substring str (add1 i)))]
+  [(>= i (string-length str)) str])
